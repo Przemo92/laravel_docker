b374679bbc868d140f0158ac9e87c5b07e23106e
Przemo <przemek.kapela92@gmail.com>
2021-01-28 11:34:55 +0100

    Little corrections

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -7,16 +7,5 @@ use Tests\TestCase;
 
 class UsersTest extends TestCase
 {
-    /** @test */
-    public function the_posts_show_route_can_be_accessed()
-    {
-        //GIVEN
-        $name = 'Tomek';
-        //WHEN
-        $response = $this->get('/palindromes/' . $name);
-
-        //THEN
-        $response->assertStatus(200)
-            ->assertSeeText('Tomek');
-    }
+//
 }
